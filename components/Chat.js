96d7d2d6f4fe7aa5e12d33c2be8309f910e9ff2b
Alexandre <Hydrark97@gmail.com>
2021-02-04 11:37:11 +0100

    Messagerie OK

--- components/Chat.js
+++ components/Chat.js
@@ -1,41 +1,82 @@
 import React, { Component, useState, useEffect } from 'react';
-import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Pressable } from 'react-native';
+import { FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View, Image, TouchableOpacity, Pressable, KeyboardAvoidingView } from 'react-native';
 import { GiftedChat } from 'react-native-gifted-chat'
 import Fire from './Fire';
 
-export default function Chat({route}) {
+// export default function Chat({route}) {
     
-    const navigationOptions = ({route}) => ({
-        title: (route.params || {}).name || 'Chat!', 
-    })
+//     const navigationOptions = ({route}) => ({
+//         title: (route.params || {}).name || 'Chat!', 
+//     })
     
-    const [messages, setMessages] = useState([])
+//     const [messages, setMessages] = useState([])
     
-    const user = {name:route.params.name,
-                _id: Fire.shared.uid,}
+//     const user = {name:route.params.name,
+//                 _id: Fire.uid,}
         
-    
 
     
-    
-    
-    useEffect(() => {
-        Fire.shared.on(message => {
-            this.setState(previousState => ({
-                messages:GiftedChat.append(previousState.messages, message)
-            }))
-        })
-        return () => {
-            Fire.shared.off()
-        }
-    })
-    return(
-        <GiftedChat
-        messages={messages}
-        onSend={Fire.shared.send}
-        user={user} />
-        ) 
+//     useEffect(() => {
+//         // Fire.shared.on(message => {
+//         //     this.setState(previousState => ({
+//         //         messages:GiftedChat.append(previousState.messages, message)
+//         //     }))
+//         // })
+//         // return () => {
+//         //     Fire.shared.off()
+//         // }
+//         Fire.get(message => setMessages(previous => ({
+//             messages: GiftedChat.append(previous.message, message)
+//         }))
+//         )
+//         console.log("OKBONJOUR")
+//         return () => {
+//             Fire.off()
+//         }
+//     })
+//     return(
+//         <GiftedChat
+//         //messages={messages}
+//         onSend={Fire.send}
+//         user={user} />
+//         ) 
         
+// }
+
+export default class Chat extends React.Component {
+
+    state = {
+        messages: []
+    }
+
+    get user(){
+        return {
+            _id: Fire.uid,
+            name: this.props.route.params.name
+        }
+    }
+
+    componentDidMount(){
+        Fire.get(message => this.setState(previous => ({
+            messages: GiftedChat.append(previous.message, message)
+            }))
+            )
+    }
+
+    componentWillUnmount(){
+        Fire.off()
+    }
+
+    render(){
+        return (
+            <SafeAreaView style={{flex:1}}>
+                <GiftedChat 
+                messages={this.state.messages}
+                onSend={Fire.send} 
+                user={this.user} />    
+            </SafeAreaView>
+        )
+    }
 }
 
 
--- components/Fire.js
+++ components/Fire.js
@@ -4,97 +4,154 @@ import * as firebase from 'firebase'
 class Fire {
     constructor(){
         this.init()
-
-        this.observAuth()
+        this.checkAuth()
     }
 
     //Authentification à la base firebase
-    observAuth = () =>{
-        firebase.auth().onAuthStateChanged(this.onAuthStateChanged)
-    }
-
-    //Précision de l'authentification anonyme
-    onAuthStateChanged = user => {
-        if(!user){
-            try{
+    checkAuth = () => {
+        firebase.auth().onAuthStateChanged(user => {
+            console.log(user)
+            if(user === null){
                 firebase.auth().signInAnonymously()
-            } catch ({message}){
-                alert(message)
             }
-        }
+        })
+    }
+    // observAuth = () =>{
+    //     firebase.auth().onAuthStateChanged(this.onAuthStateChanged)
+    // }
+
+    send = messages => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            }
+            this.db.push(message)
+        })
     }
 
-    //Créer la référence à la base de données des "messages"
-    get ref(){
-        return firebase.database().ref('messages')
-    }
+    parse = message => {
+        const {user, text, timestamp} = message.val()
+        const {key: _id} = message
+        const createdAt = new Date(timestamp)
 
-    //Récupérer les 20 derniers messages et envoyé les nouveaux pour être traiter et afficher 
-    on = callback => 
-        this.ref
-            .limitToLast(20)
-            .on('child_added', snapshot => callback(this.parse(snapshot))) 
-    
-    //Fonction de définition d'un message pouvant être accepté par "GiftedChat"   
-    parse = snapshot => {
-        const {timestamp: numberStamp, text, user} = snapshot.val()
-        const {key: _id} = snapshot
-        
-        const message = {
+        return {
             _id,
-            timestamp,
+            createdAt,
             text,
-            user,
+            user
         }
-        const timestamp = new Date(numberStamp)
-
-        return message
     }
 
-    //Fonction pour se déconnecter de la base
+    get = (callback) => {
+        this.db
+            .limitToLast(20)
+            .on('child_added', snapshot => callback(this.parse(snapshot)))
+    }
+
     off(){
-        this.ref.off()
+        this.db.off()
     }
 
-    //récupérer le user id
+    get db(){
+        return firebase.database().ref("messages")
+    }
+
     get uid(){
         return (firebase.auth().currentUser || {}).uid
     }
 
-    //récupérer le timestamp pour sauvegarder les messages
-    get timestamp(){
-        return firebase.database.ServerValue.TIMESTAMP
-    }
+    // //Précision de l'authentification anonyme
+    // onAuthStateChanged = user => {
+    //     if(!user){
+    //         try{
+    //             firebase.auth().signInAnonymously()
+    //         } catch ({message}){
+    //             alert(message)
+    //         }
+    //     }
+    // }
 
-    //Fonction d'envoie du message
-    send = messages => {
-        for(let i = 0; i<messages.length; i++){
-            const {text, user} = messages[i]
-            const message = {
-                text,
-                user,
-                timestamp: this.timestamp,
-            }
-            this.append(message)
-        }
-    }
+    // //Créer la référence à la base de données des "messages"
+    // get ref(){
+    //     return firebase.database().ref('messages')
+    // }
 
-    //append = message => this.ref.push(message)
-    append = message => alert(message.user.name)
+    // //Récupérer les 20 derniers messages et envoyé les nouveaux pour être traiter et afficher 
+    // on = callback => {
+    //     // alert(snapshot)
+    //     this.ref
+    //         .limitToLast(20)
+    //         .on('child_added', snapshot => callback(this.parse(snapshot)))
+    // } 
+    
+    // //Fonction de définition d'un message pouvant être accepté par "GiftedChat"   
+    // parse = snapshot => {
+    //     const {timestamp: numberStamp, text, user} = snapshot.val()
+    //     const {key: _id} = snapshot
+        
+    //     const message = {
+    //         _id,
+    //         timestamp,
+    //         text,
+    //         user,
+    //     }
+    //     const timestamp = new Date(numberStamp)
+
+    //     return message
+    // }
+
+    // //Fonction pour se déconnecter de la base
+    // off(){
+    //     this.ref.off()
+    // }
+
+    // //récupérer le user id
+    // get uid(){
+    //     return (firebase.auth().currentUser || {}).uid
+    // }
+
+    // //récupérer le timestamp pour sauvegarder les messages
+    // get timestamp(){
+    //     return firebase.database.ServerValue.TIMESTAMP
+    // }
+
+    // //Fonction d'envoie du message
+    // send = messages => {
+    //     for(let i = 0; i<messages.length; i++){
+    //         const {text, user} = messages[i]
+    //         const message = {
+    //             text,
+    //             user,
+    //             timestamp: this.timestamp,
+    //         }
+    //         this.append(message)
+    //     }
+    // }
+
+    // append = message => this.ref.push(message)
+    // //append = message => alert(message.user.name)
 
 
     //Définition des paramètres de connexion à Firebase
-    init = () => firebase.initializeApp ({
-        apiKey: "AIzaSyCBg_c3PPWobQJEoQcHz4-u490TDxEzDHk",
-        authDomain: "cooksender.firebaseapp.com",
-        databaseURL: "https://cooksender-default-rtdb.firebaseio.com",
-        projectId: "cooksender",
-        storageBucket: "cooksender.appspot.com",
-        messagingSenderId: "150526770311",
-        appId: "1:150526770311:web:c7c79613ea3b855431cc22",
-        measurementId: "G-JHV569NN87"
-    })
+    init = () =>{
+        if(!firebase.apps.length){
+            firebase.initializeApp ({
+                apiKey: "AIzaSyCBg_c3PPWobQJEoQcHz4-u490TDxEzDHk",
+                authDomain: "cooksender.firebaseapp.com",
+                databaseURL: "https://cooksender-default-rtdb.firebaseio.com",
+                projectId: "cooksender",
+                storageBucket: "cooksender.appspot.com",
+                messagingSenderId: "150526770311",
+                appId: "1:150526770311:web:c7c79613ea3b855431cc22",
+                measurementId: "G-JHV569NN87"
+            })
+
+        }
+
+    } 
 }
 
-Fire.shared = new Fire()
-export default Fire
+//Fire.shared = new Fire()
+export default new Fire()
